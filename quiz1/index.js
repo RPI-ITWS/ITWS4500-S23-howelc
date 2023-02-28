346d28c989f1cb0d7caae7a8f2c646fdb073d44f
CianHowell <91489341+CianHowell@users.noreply.github.com>
2023-02-28 12:39:16 -0500

    quiz1push

---
+++ quiz1/index.js
@@ -0,0 +1,26 @@
+// Global variables
+const fs = require('fs/promises');
+const express = require('express');
+const cors = require('cors');
+// Initialize express
+const app = express();
+// Set up cors
+app.use(cors({ origin: 'http://localhost:3000' }));
+// Set up express serve static files
+app.use('/quiz1', express.static('./public'));
+
+app.get('/uni/:name', (req, res) => {
+    fetch('http://universities.hipolabs.com/search?name=' + req.params['name'])
+        .then(response => response.json())
+        .then(data => res.json(data));
+});
+
+app.get('/uni/:name/:country', (req, res) => {
+    fetch('http://universities.hipolabs.com/search?name=' + req.params['name'] +"&country=" + req.params['country'])
+        .then(response => response.json())
+        .then(data => res.json(data));
+});
+//Listen on port 3000
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
